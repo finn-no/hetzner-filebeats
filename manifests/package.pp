e1b9f445155ab0c32b607a9cdb81e27bd8535c77
Aryan TaheriMonfared <aryan.taherimonfared@schibsted.com>
2018-03-27 11:03:30 +0200

    Use port 80 on the sks-keyservers, to bypass possible firewall restrictions.

--- manifests/package.pp
+++ manifests/package.pp
@@ -10,7 +10,7 @@ class filebeats::package {
         repos       => 'main',
         key         => {
           'id'        => '46095ACC8548582C1A2699A9D27D666CD88E42B4',
-          'server'    => 'pool.sks-keyservers.net',
+          'server'    => 'hkp://p80.pool.sks-keyservers.net:80',
         },
         include     => {
           'deb' => true,
